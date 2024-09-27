ce8b9d497d6c30ee9840cbf99ad28ffa11760336
PlaySkyHD <nic14markfort@gmail.com>
2024-09-27 20:29:48 +0200

    Sessions

--- backend/was_geht_muenster_bff/src/main/java/ms/wasgeht/session/controller/SessionController.java
+++ backend/was_geht_muenster_bff/src/main/java/ms/wasgeht/session/controller/SessionController.java
@@ -1,9 +1,15 @@
 package ms.wasgeht.session.controller;
 
 // SessionController.java
+import ms.wasgeht.activity.category.ActivityCategoryType;
+import ms.wasgeht.exceptions.AbstractMSHackException;
 import ms.wasgeht.session.SessionModel;
+import ms.wasgeht.session.dto.SessionRequestDto;
 import ms.wasgeht.session.service.SessionService;
+import ms.wasgeht.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,20 +21,31 @@ public class SessionController {
 
     @Autowired
     private SessionService sessionService;
+    @Autowired
+    private UserService userService;
 
     @GetMapping
-    public ResponseEntity<?> getSessions() {
+    public ResponseEntity<?> getSessions(@RequestParam(required = false) final ActivityCategoryType category, @RequestParam(defaultValue = "1") final int page, @RequestParam(defaultValue = "10") final int size) {
         return ResponseEntity.ok(sessionService.findAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getSessionById(@PathVariable UUID id) {
-        return ResponseEntity.ok(sessionService.findById(id));
+        try{
+            return this.sessionService.getSession(id).toResponse(HttpStatus.OK);
+        }catch (AbstractMSHackException e){
+            return e.toResult();
         }
+    }
 
     @PostMapping
-    public ResponseEntity<?> createSession(@RequestBody SessionModel sessionModel) {
-        return ResponseEntity.ok(sessionService.save(sessionModel));
+    public ResponseEntity<?> createSession(final @RequestHeader(name = "Authorization") String sessionToken, @RequestBody SessionRequestDto requestDto) {
+        try{
+            this.sessionService.createSession(this.userService.getUserBySessionToken(sessionToken), requestDto);
+            return new ResponseEntity<>("Created", HttpStatus.CREATED);
+        }catch (AbstractMSHackException e){
+            return e.toResult();
+        }
     }
 
 }
